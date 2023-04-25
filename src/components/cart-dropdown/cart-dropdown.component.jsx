9c6850a8a8dbc39989e2afd7fbad622c0de2c6a7
l-ram <larryramjutton@gmail.com>
2023-04-25 15:13:11 +0200

    wip

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -5,11 +5,17 @@ import { CartContext } from '../../contexts/cart.context'
 import { Button } from '../button/button.component'
 import CartItem from '../cart-item/cart-item.component'
 import './cart-dropdown.styles.scss'
+import { useNavigate } from 'react-router-dom'
 
 const CartDropDown = () => {
 
     const { cartItems } = useContext(CartContext)
+    const navigate = useNavigate();
 
+    const goToCheckoutHandler = () => {
+        navigate('/checkout')
+    }
+    
     return (
         <div className='cart-dropdown-container'>
             <div className='cart-items'>
@@ -21,7 +27,7 @@ const CartDropDown = () => {
                     <span className='empty-message'>Your Cart is Empty</span>
                 )}
             </div>
-            <Button value={'To checkout'}></Button>
+            <Button onClick={goToCheckoutHandler}>To checkout</Button>
         </div>
     )
 }
--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -1,47 +1,76 @@
 import { createContext, useEffect, useState } from 'react';
 
-export const addCartItem = (cartItems, productToAdd) => { 
+export const addCartItem = (cartItems, productToAdd) => {
 
     const existingCartItem = cartItems.find(
         (cartItem) => cartItem.id === productToAdd.id
     );
 
     if (existingCartItem) {
-        return cartItems.map((cartItem) => 
-            
+        return cartItems.map((cartItem) =>
+
             cartItem.id === productToAdd.id
-                ? { ...cartItem, quantity: cartItem.quantity + 1}
-                : cartItem            
-            );
+                ? { ...cartItem, quantity: cartItem.quantity + 1 }
+                : cartItem
+        );
     }
 
-    return [...cartItems, {...productToAdd, quantity:1}];
+    return [...cartItems, { ...productToAdd, quantity: 1 }];
 };
+
+export const removeCartItem = (cartItems, cartItemToRemove) => {
+    // find the cart item to remove
+
+    const existingCartItem = cartItems.find(
+        (cartItem) => cartItem.id === cartItemToRemove.id
+    );
+
+    // quantity is === to 1?
+
+    if (existingCartItem.quantity === 1) {
+        return cartItems.filter(cartItem => cartItem.id !== cartItemToRemove.id);
+    }
+
+    // return back cartIitems with matching cart item with reduced quantity
+
+    return cartItems.map((cartItem) =>
+
+    cartItem.id === cartItemToRemove.id
+        ? { ...cartItem, quantity: cartItem.quantity - 1 }
+        : cartItem
+);
+
+}
 
 export const CartContext = createContext({
     isCartOpen: false,
-    setIsCartOpen: () => {},
+    setIsCartOpen: () => { },
     cartItems: [],
-    addItemToCart: () => {},
+    addItemToCart: (cartItem) => { },
+    removeItemFromCart: (cartItem) => { },
     cartCount: 0
 });
 
-export const CartProvider = ({children}) => {
+export const CartProvider = ({ children }) => {
     const [isCartOpen, setIsCartOpen] = useState(false);
     const [cartItems, setCartItems] = useState([]);
     const [cartCount, setCartCount] = useState(0);
 
-    useEffect(()=> {
-        const newCartCount = cartItems.reduce((total, cartItem ) =>
+    useEffect(() => {
+        const newCartCount = cartItems.reduce((total, cartItem) =>
             total + cartItem.quantity, 0)
-            setCartCount(newCartCount);
-        }, [cartItems])
+        setCartCount(newCartCount);
+    }, [cartItems])
 
 
     const addItemToCart = (product) =>
         setCartItems(addCartItem(cartItems, product));
 
-    const value = {isCartOpen, setIsCartOpen, addItemToCart, cartItems, cartCount}
+    const removeItemFromCart = (cartItemToRemove) => {
+        setCartItems(removeCartItem(cartItems, cartItemToRemove));
+    };
+
+    const value = { isCartOpen, setIsCartOpen, addItemToCart, cartItems, cartCount, removeItemFromCart }
 
     return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
 }
